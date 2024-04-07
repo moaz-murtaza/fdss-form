a0a16a64ce7085d72485403d4734fdd43e5961f9
Moaz Murtaza <123573245+moaz-murtaza@users.noreply.github.com>
2024-04-08 00:51:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function toggleFormNextToCheckbox(checkbox) {
     var formDiv = checkbox.nextElementSibling;
     if (formDiv && formDiv.classList.contains("data")) {
-        formDiv.style.display = checkbox.checked ? "block" : "none"; 
+        formDiv.style.display = checkbox.checked ? "block" : "none";
     }
 }
 
@@ -9,8 +9,8 @@ function filterFormBlocks() {
     var selectedBatch = parseInt(document.getElementById('user-batch').value, 10);
     var selectedDegree = document.getElementById('degree').value;
     var formBlocks = document.querySelectorAll('.form-block');
-    
-    formBlocks.forEach(function(block) {
+
+    formBlocks.forEach(function (block) {
         var blockBatch = parseInt(block.getAttribute('data-batch'), 10);
         var blockDegree = block.getAttribute('data-degree');
 
@@ -22,10 +22,10 @@ function filterFormBlocks() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     var forms = document.querySelectorAll('.data');
-    forms.forEach(function(form) {
+    forms.forEach(function (form) {
         form.style.display = 'none';
     });
 
@@ -33,12 +33,47 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('degree').addEventListener('change', filterFormBlocks);
 
     var checkboxes = document.querySelectorAll('.form-block .check');
-    
-    checkboxes.forEach(function(checkbox) {
-        checkbox.addEventListener('change', function() {
+
+    checkboxes.forEach(function (checkbox) {
+        checkbox.addEventListener('change', function () {
             toggleFormNextToCheckbox(checkbox);
         });
     });
 
     filterFormBlocks();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const loader = document.querySelector('.loader');
+    const contentToHide = document.querySelector('#selectors');
+    const formToHide = document.querySelector('.form');
+    const headerToHide = document.querySelector('.header');
+    const footerToHide = document.querySelector('.footer');
+    const missionToHide = document.querySelector('.mission');
+
+    function hideContent() {
+        contentToHide.style.display = 'none';
+        formToHide.style.display = 'none';
+        headerToHide.style.display = 'none';
+        footerToHide.style.display = 'none';
+        missionToHide.style.display = 'none';
+    }
+
+    function showContent() {
+        contentToHide.style.display = '';
+        formToHide.style.display = '';
+        headerToHide.style.display = '';
+        footerToHide.style.display = '';
+        missionToHide.style.display = '';
+    }
+
+    loader.style.display = 'block';
+    hideContent();
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+        showContent();
+    }, 3000);
+});
+
+
